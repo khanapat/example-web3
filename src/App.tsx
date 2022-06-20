f8951f97e8ea4b26b39033f0baf776c37c64045d
Khanapat.a <k.apiwattanawong@gmail.com>
2022-06-20 22:46:12 +0700

    feat: connect smartcontract

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
 import React from 'react';
 import logo from './logo.svg';
+import { useWeb3React, UnsupportedChainIdError } from "@web3-react/core";
+import { injected } from "./components/wallet/connector";
+import { ethers } from "ethers";
+import TestContract from "./artifacts/Test/Test.json";
+
 import './App.css';
 
 function App() {
+  const { active, account, library, connector, activate, deactivate } = useWeb3React<ethers.providers.Web3Provider>();
+
+  const connect = async () => {
+    try {
+      await activate(injected, async (error: Error) => {
+        console.log(error);
+        if (error instanceof UnsupportedChainIdError) {
+          console.log(error);
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const disconnect = async () => {
+    try {
+      deactivate();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const sendTxn = async () => {
+    if (window.ethereum) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(TestContract.address, TestContract.abi, provider);
+
+      const signer = provider.getSigner();
+      const tx: ethers.providers.TransactionResponse = await contract.connect(signer).setA(999);
+      await tx.wait(1);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={connect}>Connect to MetaMask</button>
+      <button onClick={sendTxn}>Set A</button>
     </div>
   );
-}
+};
+
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Web3ReactProvider } from "@web3-react/core";
+import { ethers } from "ethers";
+// import Web3 from "web3"
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+function getLibrary(provider: ethers.providers.ExternalProvider | ethers.providers.JsonRpcFetchFunc): ethers.providers.Web3Provider {
+  return new ethers.providers.Web3Provider(provider);
+}
+
+// export const getLibrary = (provider: Web3): Web3 => {
+//   return provider
+// }
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <App />
+    </Web3ReactProvider>
+  </React.StrictMode >,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
